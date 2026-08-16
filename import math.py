d282e4f07bb6d1df307fbec9aca4eecff78074cf
francapolack <frmuuoo@gmail.com>
2026-08-15 21:09:36 -0300

    eu entendi la formula :D

--- import math.py
+++ import math.py
@@ -4,11 +4,12 @@ r=6871
 ascension=20
 tasa=15
 
-conversion=math.radians(ascension*tasa)
-
+conversion=math.radians(300)
+print(conversion)
+print(f"{"SEPARADOR":-^30}")
 cosine=math.cos(conversion)
 print(f"Cosino en radianes:{cosine}")
 print(f"{"SEPARADOR":-^30}")
-cosino=math.degrees(cosine)
-print(f"Cosino en grados:{cosino}")
+x=r*cosine*cosine
+print(f"EjeX:{cosine}")
 print(f"{"SEPARADOR":-^30}")
