b0e561f5824e9920c4e85884e42e87ea8eb67ffd
francapolack <frmuuoo@gmail.com>
2026-08-19 13:23:11 -0300

    integrado bieeeen :D falta fisico nomas #help

--- main.py
+++ main.py
@@ -2,39 +2,63 @@
 #aca escribe franca su fiel servidora :))
 from ultralytics import YOLO
 import math
-import cv2
-import urllib.request
+from matplotlib import pyplot as plt
 
 url="http://cam.jgp"#TODO:configure l8r to my wifi
 
 #lista completa de los datos de las constelaciones,orden es: 
-#1)Nombre en ingles de la clase,2)Nombre en español,3)Ascencion recta
+#1)Nombre en ingles de la clase 2)Nombre en español 3)Ascencion recta
 constelaciones_datos=["scorpius","Escorpio",18,"canis_major","Canis Mayor",7,"pleiades",4,"sagittarius","Sagitario",20]
 tierra_mas_orbita=6871 #6371km de distancia desde el radio de la tierra + 500km de lanzamiento del satelite
-    
+def mapita(x,y):
+    equis=[0,x]
+    i=[0,y]
+
+    plt.style.use('dark_background')
+    fig,ax=plt.subplots(figsize=(12,8))
+
+    Tierra=plt.Circle((0,0),800,color='green')
+    ax.add_patch(Tierra)
+
+    ax.plot(equis,i,'o-',markersize=8,color='red')
+
+    ax.set_aspect('equal')
+    ax.grid(True,linestyle='dashed',alpha=0.5)
+
+    ax.set_xlim(40000,-40000)
+    ax.set_ylim(40000,-40000)
+
+    ax.set_xlabel("EjeX desde la tierra")
+    ax.set_ylabel("EjeY desde la tierra")
+
+    plt.tight_layout()
+
+    plt.savefig('orbita.png',dpi=1000)
+    plt.show()
+
 def calculo_ejes(ascension,nombre):
     #convertimos las horas de ascension en grados
     tasa_conversion=15
     grado_constelacion=ascension*tasa_conversion
     #coseno y seno de las horas en grados 
     cos=math.cos(math.radians(grado_constelacion))#dice raidans porq la funcion math requiere que el angulo sea radianes no grados (si lo ponemos sin convertir nos arruina los datos)
-    cos_grado=math.degrees(cos)
     sin=math.sin(math.radians(grado_constelacion))
-    sin_grado=math.degrees(sin)
     #calculo de ejeX
-    ejex=tierra_mas_orbita*cos_grado*cos_grado
+    ejex=tierra_mas_orbita*cos*cos
     #calculo de ejeY
-    ejey=tierra_mas_orbita*cos_grado*sin_grado
+    ejey=tierra_mas_orbita*cos*sin
     #calculo de ejeZ
-    ejez=tierra_mas_orbita*sin_grado
+    ejez=tierra_mas_orbita*sin
     print("------------------")
     print(nombre)
     print("------------------")
-    print( ejex)
+    print(ejex)
     print("------------------")
     print(ejey)
     print("------------------")
     print(ejez)
+    mapita(ejex,ejey)
+
 
 
 #lo estoy conteniendo en una funcion para ver si lo llamo cuando la ESP32 se refresque
